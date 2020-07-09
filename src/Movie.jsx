a79fd2fc32988e7cb80d302f7fcac9b2110e071e
mimi <mimsgh2018@gmail.com>
2020-07-09 14:47:58 +0000

    added a like component and handled liked event

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "./services/fakeMovieService";
+import Like from "./Like";
 class Movie extends Component {
   state = {
     movies: getMovies(),
@@ -8,6 +9,14 @@ class Movie extends Component {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
   };
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
   render() {
     const { length: count } = this.state.movies;
     if (count === 0) return <p>There are no movies in the database.</p>;
@@ -22,6 +31,7 @@ class Movie extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -31,6 +41,12 @@ class Movie extends Component {
                 <td> {movie.genre.name} </td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
